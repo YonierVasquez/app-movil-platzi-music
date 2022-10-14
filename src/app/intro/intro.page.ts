0fc01b94d0d2d04bdee0ce748d763bdeec37b6c2
Yonier Vasquez Marin <yonier.marinm@opitech.com.co>
2022-10-14 10:53:13 -0500

    cerrar slide para ir al home

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  constructor(
+    private router: Router,
+    private storage: Storage
+  ) { }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+
+  finish() {
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl('/home');
+  }
+
+}
