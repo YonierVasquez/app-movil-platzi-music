5df6779c0e5df6245577a0cc14f742c7db1ab1b2
Yonier Vasquez Marin <yonier.marinm@opitech.com.co>
2022-10-14 11:43:01 -0500

    pagina login agregada

---
+++ src/app/guards/intro/intro.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntroGuard implements CanActivate {
+
+  constructor(
+    private storage: Storage,
+    private router: Router
+  ) {
+    this.storage.create();
+  }
+
+  async canActivate(): Promise<boolean> {
+    const isIntroShowed = await this.storage.get('isIntroShowed');
+    if (isIntroShowed) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/intro');
+      return false;
+    }
+  }
+  
+}
