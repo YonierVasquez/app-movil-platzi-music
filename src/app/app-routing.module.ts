bac753287be3ba2e00c7ee9362c96abb9b728bd0
Yonier Vasquez Marin <yonier.marinm@opitech.com.co>
2022-10-15 19:04:36 -0500

    menu iniciado con su página principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,8 @@ import { LoginGuard } from './guards/login/login.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
-    canActivate: [LoginGuard, IntroGuard]
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'menu/home',
     pathMatch: 'full'
   },
   {
@@ -25,6 +20,11 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule),
+    canActivate: [LoginGuard, IntroGuard]
   },
 ];
 
